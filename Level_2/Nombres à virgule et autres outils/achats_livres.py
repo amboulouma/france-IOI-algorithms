219bb90dce773de33779e2bf79ad26e9ee85a29b
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-03-14 13:38:10 +0100

    Added caserne_pompiers

---
+++ Level_2/Nombres à virgule et autres outils/achats_livres.py
@@ -0,0 +1,4 @@
+Votre programme doit commencer par lire la somme d'argent dont vous disposez et lira ensuite le prix 
+d'un livre. Il devra ensuite afficher un entier, le plus grand nombre de livres qu'il vous est possible
+ d'acheter avec cette somme d'argent.
+

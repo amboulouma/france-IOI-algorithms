e02bcf540df1d3e5cde96631e961c08b523ae7e4
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-03-15 00:54:20 +0100

    Added petites_fiches_grand_travail

--- Level_2/Découverte des Tableaux/choix_des_emplacements.py
+++ Level_2/Découverte des Tableaux/choix_des_emplacements.py
@@ -5,3 +5,12 @@ for i in range(n):
     tab[m] = i
 for i in range(n):
     print(tab[i])
+
+
+    for(int i=0;i<N;++i)
+    {
+      getline(cin,s1);
+      getline(cin,s2);
+      cout<<s2<<endl;
+      cout<<s1<<endl;
+    }

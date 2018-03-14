0dbbbe1be35e88c00c6c8c49021b27902c8503b5
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-03-14 22:41:37 +0100

    Added convertion_distances

--- Level_2/Nombres à virgule et autres outils/achats_livres.py
+++
@@ -1,4 +0,0 @@
-Votre programme doit commencer par lire la somme d'argent dont vous disposez et lira ensuite le prix 
-d'un livre. Il devra ensuite afficher un entier, le plus grand nombre de livres qu'il vous est possible
- d'acheter avec cette somme d'argent.
-

a3f07807247db79955835109d265a2ba2c5a9049
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-03-14 23:39:28 +0100

    Added etude_de_marche

---
+++ Level_2/Découverte des Tableaux/etude_de_marche.py
@@ -0,0 +1,10 @@
+n, m = int(input()), int(input())
+tab = [0]*m
+for i in range(m):
+    tab[i] = int(input())
+for i in range(n):
+    cnt = 0
+    for j in range(m):
+        if tab[j] == i:
+            cnt += 1
+    print(cnt)
